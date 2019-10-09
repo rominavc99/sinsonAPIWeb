faca43cbbeaa294114fa900851f36d81fc486884
rominavc99 <procrom1999@gmail.com>
2019-10-09 09:12:07 -0700

    css modificado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const secondnamee = document.getElementById('secondname1');
 const lastnamee = document.getElementById('lastname1');
 const agee = document.getElementById('age1');
 const photoo = document.getElementById('photo1');
+const descriptioon = document.getElementById('description1')
 
 const apiUrl = 'https://localhost:5001/simpsons/character'
 
@@ -11,13 +12,17 @@ const apiResponse = async url => {
     const data = await response.json();
     console.log(data);
 
-    const {name, secondName, lastName, age, photo} = data;
+    const {name, secondName, lastName, age, description} = data;
+    const imgpic = data.photo;
 
 
+
+    photoo.src = imgpic;
     agee.innerText = age;
     namee.innerText = name;
     secondnamee.innerText = secondName;
     lastnamee.innerText = lastName;
+    descriptioon.innerText = description;
   
 };
 
